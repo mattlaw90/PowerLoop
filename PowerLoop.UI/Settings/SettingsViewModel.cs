b3b5fb93801908a3271271fd650227115074efba
mattlaw90 <35229746+mattlaw90@users.noreply.github.com>
2022-09-12 21:46:53 +0100

    Add settings display

--- PowerLoop.UI/Settings/SettingsViewModel.cs
+++ PowerLoop.UI/Settings/SettingsViewModel.cs
@@ -4,10 +4,44 @@
 
 namespace PowerLoop.UI.Settings
 {
+    using System.Collections.Generic;
     using CommunityToolkit.Mvvm.ComponentModel;
 
     public class SettingsViewModel : ObservableObject
     {
+        public SettingsViewModel()
+        {
+            this.LoopItems.Add(new LoopItem()
+            {
+                Order = 1,
+                Type = LoopItemType.Image,
+                Path = "images/ClubCrest.png",
+            });
+
+            this.LoopItems.Add(new LoopItem()
+            {
+                Order = 2,
+                Type = LoopItemType.Web,
+                Path = "https://www.englandrugby.com/fixtures-and-results/search-results?team=14247&season=2022-2023#results",
+            });
+
+            this.LoopItems.Add(new LoopItem()
+            {
+                Order = 3,
+                Type = LoopItemType.Web,
+                Path = "https://www.englandrugby.com/fixtures-and-results/search-results?team=14247&season=2022-2023#table",
+            });
+
+            this.LoopItems.Add(new LoopItem()
+            {
+                Order = 4,
+                Type = LoopItemType.Web,
+                Path = "https://www.englandrugby.com/fixtures-and-results/search-results?team=14247&season=2022-2023#fixtures",
+            });
+        }
+
+        public List<LoopItem> LoopItems { get; } = new List<LoopItem>();
+
         public void GetSettings()
         {
 
